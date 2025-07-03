06917996015d9cdd6a1003d02b9b1c755498c0f7
MinThuKhant763 <85979978+MinThuKhant763@users.noreply.github.com>
2025-07-03 20:37:19 +0630

    Install laravel framework

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 
+import {Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './components/Home';
 import NavBar from './components/NavBar';
-
+import About from './components/About';
+import Major from './components/Major';
 function App() {
 
-
   return (
-    <>
-      <NavBar />
-      
-    </>
+    
+      <Routes>
+        <Route path="/" element={ <NavBar /> }>
+          <Route index element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/major" element={ <Major /> } />
+        </Route>
+      </Routes>
+
   )
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,41 +1,44 @@
-
+import { Outlet, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faMoon} from '@fortawesome/free-regular-svg-icons';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const NavBar = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <div className='container-fluid'>
-              <div className='col'>
-                  <a className='navbar-brand' href="#">MTU</a>
-              </div>
-              <div className='col collapse navbar-collapse' id='navbarSupportedContent'>
-                  <ul className='navbar-nav me-auto mb-2 mb-lg-2 gap-5'>
-                      <li className='nav-item'>
-                          <a className='nav-link active' href="#">Home</a>
-                      </li>
-                      <li className='nav-item'>
-                          <a className='nav-link' href="/major">Major</a>
-                      </li>
-                      <form action="" className='d-flex'>
-                          <input class="form-control me-2 w-75" type="search" placeholder="Search book" aria-label="Search" />
-                          <button class="btn btn-outline-success" type="submit">Search</button>
-                      </form>
+      <>
+          <nav className='navbar navbar-expand-lg navbar-light bg-white shadow-sm'>
+              <div className='container-fluid'>
+                  <div className='col'>
+                      <Link className='navbar-brand ms-2' to="/">MTU</Link>
+                  </div>
+                  <div className='col collapse navbar-collapse' id='navbarSupportedContent'>
+                      <ul className='navbar-nav me-auto gap-5'>
+                          <li className='nav-item'>
+                              <Link className='nav-link active' to="/">Home</Link>
+                          </li>
+                          <li className='nav-item'>
+                              <Link className='nav-link' to="/about">About</Link>
+                          </li>
+                          <li className='nav-item'>
+                              <Link className='nav-link' to="/major">Major</Link>
+                          </li>
+                          <form action="" className='d-flex align-items-center gap-2'>
+                              <input className="form-control w-75" type="search" placeholder="Search books ... " aria-label="Search" />
+                              <button className="btn btn-outline-primary" type="submit"><FontAwesomeIcon icon={ faMagnifyingGlass } /></button>
+                          </form>
 
-                      <li className='nav-item'>
-                          <a className='nav-link' href="/about">About</a>
-                      </li>
-                      <li className='nav-item align-self-center'>
-                          <FontAwesomeIcon icon={faMoon} />
-                      </li>
-                  </ul>
-              </div>
-              <div className='d-flex col justify-content-end'>
-                        <span className='bg-primary border rounded'></span>
+                      </ul>
+                  </div>
+                  <div className='d-flex col justify-content-end gap-2'>
+                      <span className='btn btn-outline-primary me-2'>
+                          <FontAwesomeIcon icon={ faMoon } />
+                      </span>
                       <div className='btn btn-primary'>Sign In</div>
-                </div>
-          </div>
-          
-    </nav>
+                  </div>
+              </div>
+
+          </nav>
+          <Outlet />
+      </>
   )
 }
 
